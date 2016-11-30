6b09934664c9585d7cf4c057aa9eebce4372e282
ydc223 <ydc223@nyu.edu>
2016-12-01 03:48:43 +0400

    css

--- client/js/dragondrop.js
+++ client/js/dragondrop.js
@@ -2,6 +2,7 @@ function dragstart_handler(ev) {
  console.log("dragStart");
  // Change the source element's background color to signify drag has started
  event.dataTransfer.dropEffect = "copy";
+ console.log(ev.target.id);
  ev.dataTransfer.setData("text", ev.target.id);
 }
 
@@ -13,8 +14,14 @@ function dragover_handler(ev) {
 }
 
 function drop_handler(ev) {
+
+  console.log(ev.target.nodeName);
+  console.log(ev.target.parentElement);
+
  console.log("Drop");
+
  ev.preventDefault();
  var data = ev.dataTransfer.getData("text");
+ var parent = ev.target.nodeName;
  ev.target.appendChild(document.getElementById(data));
 }
