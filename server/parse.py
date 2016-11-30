a8e23c6752e44f2941a7b5e3e52805d0dfc3f29b
Guyu Fan <guyufan1996@gmail.com>
2016-12-01 00:33:30 +0400

    integrate parsing into server

--- server/parse.py
+++ server/parse.py
@@ -1,18 +1,9 @@
 from urllib import request
 import xml.etree.ElementTree as ET
 
-course_url = "http://services.nyuad.nyu.edu/academics/courses"
 
-# course_xml = request.urlopen(course_url).read()
-# category_xml = request.urlopen(category_url).read()
-
-tree = ET.parse('server/course.xml')
-root = tree.getroot()
-
-big_list = []
 
 def parse_course(course):
-
 	ID = course.get("peoplesoftID")
 	title = course.find("title").text
 	description = course.find("description").text
@@ -37,18 +28,25 @@ def parse_course(course):
 
 	return ID, title, description, sem_offered, requirements, prereq
 
-# def get_ID_by_title(title):
-# 	for course in root.iterfind("course"):
-# 		if course.find("title").text == title:
-# 			return course.get("peoplesoftID")
+def get_courses():
+	course_url = "http://services.nyuad.nyu.edu/academics/courses"
 
-for child in root:
-	ID, title, description, semester, requirements, prereq = parse_course(child)
-	tmp = {}
-	tmp["ID"] = ID
-	tmp["title"] = title
-	tmp["description"] = description
-	tmp["semester"] = semester
-	tmp["requirements"] = requirements
-	tmp["prereq"] = prereq
-	big_list.append(tmp)
+	# course_xml = request.urlopen(course_url).read()
+	# category_xml = request.urlopen(category_url).read()
+
+	tree = ET.parse('course.xml')
+	root = tree.getroot()
+
+	course_list = []
+	for child in root:
+		ID, title, description, semester, requirements, prereq = parse_course(child)
+		tmp = {}
+		tmp["ID"] = ID
+		tmp["title"] = title
+		tmp["description"] = description
+		tmp["semester"] = semester
+		tmp["requirements"] = requirements
+		tmp["prereq"] = prereq
+		course_list.append(tmp)
+
+	return course_list
--- server/server.py
+++ server/server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, send_file, Response
 
 import os
+import parse
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 
-@app.route('/ad-classes', methods=['GET'])
+@app.route('/course-ad', methods=['GET'])
 def get_ad_classes():
-    return None;
+  return jsonify(parse.get_courses())
 
 if __name__ == '__main__':
-    app.run(port=2525, debug=True)
+  app.run(port=2525, debug=True)
