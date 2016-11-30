b52192cfb792ed3426956e1964f5d030a6f995b8
ydc223 <ydc223@nyu.edu>
2016-11-30 22:18:58 +0400

    Navigate between calenders

--- client/js/script.js
+++ client/js/script.js
@@ -15,3 +15,49 @@ function drop_handler (event) {
   event.preventDefault();
   console.log("dropped");
 }
+
+function previous_year(){
+  var i;
+  for( i=0; i<4; i++){
+    if (!(($("#"+i).hasClass('disabled')))){
+      break;
+    }
+  }
+  console.log(i);
+  if(i===0){
+    return;
+  }
+  else {
+    for(var j=0; j<4; j++){
+      if(j===i-1){
+        $("#"+j).removeClass('disabled');
+      }
+      if(j===i){
+        $("#"+j).addClass('disabled');
+      }
+    }
+  }
+}
+
+function next_year(){
+  var i;
+  for( i=0; i<4; i++){
+    if (!(($("#"+i).hasClass('disabled')))){
+        break;
+    }
+  }
+  console.log(i);
+  if(i===3){
+    return;
+  }
+  else {
+    for(var j=0; j<4; j++){
+      if(j===i+1){
+        $("#"+j).removeClass('disabled');
+      }
+      if(j===i){
+        $("#"+j).addClass('disabled');
+      }
+    }
+  }
+}
