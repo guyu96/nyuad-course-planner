44a51a08778314b5c3688781f4ac46d023b71e21
Guyu Fan <guyufan1996@gmail.com>
2016-12-01 00:53:18 +0400

    connect client-server

--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,15 @@
 var category = JSON.parse('{"Previous Core Curriculum":["42","43","44","45","46","47","48"],"Islamic Studies":["49"],"Language":["50","51","52","53","161"],"Art and Art History":["59","60","61","62","159","160"],"Arts and Humanities Colloquia":["63"],"Biology":["64","65","66","67"],"Chemistry":["68","69"],"Civil Engineering":["70","71"],"Computer Engineering":["72"],"Computer Science":["73","74"],"Economics":["75","76","77","78"],"Electrical Engineering":["79","80"],"Film and New Media":["81","82","83","84","85"],"Foundations of Science":["86"],"General Engineering":["87","88","89","90"],"History":["91","92","93","94","95","96","97","98","162"],"Literature and Creative Writing":["99","100","101","102","103","158"],"Mathematics":["104","105"],"Mechanical Engineering":["106","107"],"Music":["108","109","110","111","112","113","114","115","116","117"],"Philosophy":["118","119","120","121","122","123","124"],"Physics":["125"],"Political Science":["126","127","128","129","130","131","132"],"Psychology":["134","135","136","137"],"Social Research and Public Policy":["138","139","140"],"Theater":["141","142","143"],"Visual Arts":["144","145","146","147"],"Arab Crossroads":["55","56","57","58","157"],"Physical Education":["148"],"Pre-Professional Courses":["149","150","151","152","154","155","156"],"Core Curriculum":["163","164","165","166","167","168"]}');
 
+function populate_course_list() {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var courses = JSON.parse(this.responseText);
+    console.log(courses);
+  }
+  xhr.open("GET", '/course-ad');
+  xhr.send();
+}
+
 function populate_type_dropdown() {
   var dropdown = document.getElementById('course-type');
   var types = Object.keys(category);
@@ -97,4 +107,6 @@ function next_year(){
   }
 }
 
+
 populate_type_dropdown();
+populate_course_list();
--- server/server.py
+++ server/server.py
@@ -3,7 +3,9 @@ from flask import Flask, request, jsonify, send_file, Response
 import os
 import parse
 
-app = Flask(__name__, static_url_path='', static_folder='public')
+app = Flask(__name__, static_url_path='', static_folder='../client')
+
+app.add_url_rule('/', 'index', lambda: app.send_static_file('index.html'))
 
 @app.route('/course-ad', methods=['GET'])
 def get_ad_classes():
