99363f553b6eb976f8c9bd05239f088e0b2fbea4
ydc223 <ydc223@nyu.edu>
2016-12-01 12:47:15 +0400

    Printing functionality improved

--- client/js/pdf.js
+++ client/js/pdf.js
@@ -1,23 +1,31 @@
 // Default export is a4 paper, portrait, using milimeters for units
 function savetoPDF(){
+  years=["Freshman", "Sophomore", "Junior", "Senior"]
   var doc = new jsPDF()
   var counter=0;
   for (var i=0; i<4; i++){
+    doc.setFontSize(14);
+    doc.text(years[i], 10, 10+(counter*5));
+    counter++;
     var planner = document.getElementById(i);
     console.log(planner.className);
     var planner_children = planner.childNodes;
     console.log(planner_children);
     for (var j=0; j<planner_children.length; j++){
       var text = planner_children[j].textContent;
-      console.log(text);
-      doc.text(text, 10, 10+(counter*10));
-      counter++;
       var semester_children = planner_children[j].childNodes;
       for (var k=0; k<semester_children.length; k++){
-        var text = semester_children[k].textContent;
-        console.log(text);
-        doc.text(text, 10, 10+(counter*10));
+
+        var innertext = semester_children[k].textContent;
+        console.log(innertext);
+        doc.setFontSize(10);
+        doc.text(innertext, 10, 10+(counter*5));
+        console.log(counter);
         counter++;
+        if (counter > 56){
+          doc.addPage('a4');
+          counter=0;
+        }
       }
     }
   }
